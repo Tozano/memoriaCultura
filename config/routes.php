ca5a331df696ec93a523445cb4e202bde497f127
Tanguy OZANO <tanguy.ozano@gmail.com>
2023-10-05 11:38:20 +0200

    Made addContent page

--- config/routes.php
+++ config/routes.php
@@ -10,6 +10,8 @@ function getPage(){
     $lesPages['deconnexion'] = "deconnexionControleur";
     $lesPages['profil'] = "profilControleur";
     $lesPages['mycontent'] = "showMyContentControleur";
+    $lesPages['addcontent'] = "addContentControleur";
+    $lesPages['removecontent'] = "removeContentControleur";
 
     if(isset($_GET['page'])){
         $page = $_GET['page'];
@@ -17,10 +19,15 @@ function getPage(){
         $page = 'accueil';
     }
 
-    if (!isset($_SESSION['login']) && $page == 'profil') {
+    if (!isset($_SESSION['login']) && ($page == 'profil' || $page == 'mycontent' || $page == 'addcontent' || $page == 'removecontent')) {
         $page = 'accueil';
     }
 
+    if (!isset($_SESSION['role'])) {
+        if ($_SESSION['role'] != 1 && ($page == 'mycontent' || $page == 'addcontent' || $page == 'removecontent')) {
+            $page = 'accueil';
+        }
+    }
     if (!isset($lesPages[$page])){
         $page = 'accueil';
     } 
--- controleur/contentCreatorControleur.php
+++ controleur/contentCreatorControleur.php
@@ -8,8 +8,71 @@
     }
 
     function addContentControleur($db){
+        if ($_SESSION['role'] == 1) {
+            if (isset($_POST['btAddContent'])) {
+                if ($db == null) {
+                    $_SESSION['authError'] = 'Connexion impossible';
+                } else {
+                    if (isset($_POST['inputTitle'])
+                        && isset($_POST['inputDesc'])
+                        && isset($_POST['inputAddress'])
+                        && isset($_POST['inputYear'])
+                        && isset($_POST['token'])
+                        && isset($_POST['sendDate'])) {
+                            $title = $_POST['inputTitle'];
+                            $desc = $_POST['inputDesc'];
+                            $address = $_POST['inputAddress'];
+                            $year = $_POST['inputYear'];
+                            $token = filter_input(INPUT_POST, 'token', FILTER_SANITIZE_STRING);
+                            $senDate =$_POST['sendDate'];
+                            
+                            if (!$token) {
+                                echo 'Il manque un token';
+                                exit;
+                            } else {
+                                foreach ($_SESSION['tokens'] as $aToken) {
+                                    if ($aToken[0] == $senDate) {
+                                        if ($token !== $aToken[1]) {
+                                            echo 'Le token n\'est pas le bon :';
+                                        } else {
+                                            //if (verifInsertUser($pseudo, $email, $password, $passwordConf, $nom, $prenom, $shortDesc)) {
+                                                $user = new User($db);
+                                                $userData = $user->selectUserByPseudo($_SESSION['login']);
+                                                $content = new Content($db);
+                                                $r = $content->insertContent($title, $desc, $address, $year, $userData['user_id']);
+                                                
+                                                if ($r) {
+                                                    echo 'Vous êtes bien ajouté '.$title;
+                                                    header("Location: index.php?page=mycontent");
+                                                } else {
+                                                    $_SESSION['authError'] = 'Vous n\'avez pas pu ajouter du contenu';
+                                                }
+                                            //}
+                                        }
+                                    }
+                                    if ($aToken[0] >= date("Y-m-d H:i:s")) {
+                                        unset($aToken);
+                                    }
+                                }
+                            }    
+                    } else {
+                        $_SESSION['authError'] = 'Il manque une variable dans le formulaire';
+                    }
+                }
+            }
+        } else {
+            $_SESSION['authError'] = 'Vous n\'avez pas les droits';
+        }
         require_once "template/contentCreator/addContent.php";
     }
 
     function removeContentControleur($db){
+        if ($_SESSION['role'] == 1) {
+        $user = new User($db);
+        $userData = $user->selectUserByPseudo($_SESSION['login']);
+        $content = new Content($db);
+        $contentsData = $content->selectAllContentsByCreator($userData['user_id']);
+        } else {
+            $_SESSION['authError'] = 'Vous n\'avez pas les droits';
+        }
     }
