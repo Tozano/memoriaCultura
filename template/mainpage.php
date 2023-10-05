aea0e87ab5a71787e497da678fa7b92ceedd7f1f
*Kelian <kelian.dancot@epsi.fr>
2023-10-05 09:50:19 +0200

    Date

--- template/mainpage.php
+++ template/mainpage.php
@@ -34,57 +34,127 @@
     <ol>
       <li>
         <div>
-          <time>1934</time> 
+          <time>52 av. J.-C</time> Bataille de Gergovie
         </div>
       </li>
       <li>
         <div>
-          <time>1937</time> 
+          <time>476</time> Chute de l'Empire romain d'Occident
         </div>
       </li>
       <li>
         <div>
-          <time>1940</time> 
+          <time>496</time> Conversion de Clovis au christianisme
         </div>
       </li>
       <li>
         <div>
-          <time>1943</time> 
+          <time>732</time> Bataille de Poitiers (ou Tours)
         </div>
       </li>
       <li>
         <div>
-          <time>1946</time> 
+          <time>843</time> Traité de Verdun
         </div>
       </li>
       <li>
         <div>
-          <time>1956</time> 
+          <time>1066</time> Guillaume le Conquérant envahit l'Angleterre
         </div>
       </li>
       <li>
         <div>
-          <time>1957</time>
+          <time>1337-1453</time> Guerre de Cent Ans
         </div>
       </li>
       <li>
         <div>
-          <time>1967</time> 
+          <time>1429</time> Jeanne d'Arc mène les troupes françaises à la victoire à Orléans
         </div>
       </li>
       <li>
         <div>
-          <time>1977</time> 
+          <time>1515</time> François Ier devient roi de France
         </div>
       </li>
       <li>
         <div>
-          <time>1985</time> 
+          <time>1789</time> Prise de la Bastille, début de la Révolution française
         </div>
       </li>
       <li>
         <div>
-          <time>2000</time> 
+          <time>1792</time> Proclamation de la République
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1804</time> Napoléon devient empereur
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1815</time> Défaite de Napoléon à Waterloo
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1830</time> Révolution de Juillet, début de la monarchie de Juillet
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1848</time> Révolution de février, proclamation de la Deuxième République
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1852</time> Napoléon III devient empereur
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1870</time> Défaite française lors de la guerre franco-prussienne
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1889</time> Exposition universelle à Paris, Tour Eiffel inaugurée
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1914-1918</time> Première Guerre mondiale
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1940</time> Occupation allemande de la France pendant la Seconde Guerre mondiale
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1944</time> Libération de la France
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1957</time> Signature du traité de Rome, début de la CEE
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1968</time> Les événements de Mai 1968
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>1989</time> Célébration du bicentenaire de la Révolution française
+        </div>
+      </li>
+      <li>
+        <div>
+          <time>2002</time> Passage à l'euro
         </div>
       </li>
       <li></li>
@@ -99,6 +169,9 @@
       </button>
     </div>
   </section>
+
+
+
 </body>
 
 <?php
